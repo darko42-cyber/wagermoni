980d238bf7bb37ff9d6c1646a8356992523538d6
EmmanuelDarko <edarko322@gmail.com>
2023-08-11 22:18:28 +0000

    all

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "https://wagermoni-ap.onrender.com",
+  baseURL: "https://wagermoni-api.onrender.com",
   headers: {},
   withCredentials: true,
 });
