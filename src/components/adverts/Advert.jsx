2e09199fbf003dabd0b6a698e655db05b5ae2e66
EmmanuelDarko <edarko322@gmail.com>
2023-08-30 19:18:42 +0000

    advert

--- src/components/adverts/Advert.jsx
+++ src/components/adverts/Advert.jsx
@@ -5,8 +5,8 @@ const Advert = () => {
   return (
     <div className='flex items-center justify-center'>
       <div className='w-max rounded-b-3xl bg-primary text-white fixed top-[70px] text-base z-[999] p-3 shadow-md flex items-center justify-center'>
-        <Link to='tel:0204079169'>
-          whatsapp <sup className='text-xs'>click me</sup>
+        <Link to='tel:0204079169' className='text-white'>
+          whatsapp
           <i className='ri-whatsapp-line text-green-500'></i>
           <span className='text-lg font-bold inline-block ml-1'>
             0204079169
