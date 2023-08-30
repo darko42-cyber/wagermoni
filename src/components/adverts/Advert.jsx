8e2ceae46230a3fae0b15d3bff6b04d5a6b4d6ec
EmmanuelDarko <edarko322@gmail.com>
2023-08-30 19:12:54 +0000

    advert

--- src/components/adverts/Advert.jsx
+++ src/components/adverts/Advert.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 const Advert = () => {
   return (
     <div className='flex items-center justify-center'>
-      <Link to='tel:0204079169'>
-        <div className='w-max bg-primary text-white fixed top-[70px] text-md z-[999] p-3 shadow-md flex items-center justify-center'>
-          whatsapp{" "}
+      <div className='w-max rounded-b-3xl bg-primary text-white fixed top-[70px] text-base z-[999] p-3 shadow-md flex items-center justify-center'>
+        <Link to='tel:0204079169'>
+          whatsapp <sup className='text-xs'>click me</sup>
           <i className='ri-whatsapp-line text-green-500'></i>
-          <span className='text-xl font-bold inline-block ml-1'>
+          <span className='text-lg font-bold inline-block ml-1'>
             0204079169
           </span>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </div>
   );
 };
