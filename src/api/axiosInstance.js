b15b1f8f9ece9d028b50cd8a4445d36d64d59fe8
EmmanuelDarko <edarko322@gmail.com>
2023-08-30 18:56:54 +0000

    axios instance fixed

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "https://wagermoni-api.onrender.com",
+  baseURL: "https://wagermoni-api.onrender.com/api",
   headers: {
     authorization: `Bearer ${localStorage.getItem(
       "token"
